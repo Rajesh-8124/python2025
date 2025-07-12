9f0739f6b8a7d174c3259acec86fab89da9b8eb3
Rajesh-8124 <rajesh100998kumar@gmail.com>
2025-07-13 00:58:38 +0530

    third

--- whatsapp.py
+++ whatsapp.py
@@ -1,8 +1,19 @@
 import pywhatkit
+import streamlit as st
 
-number = input("Enter recevier's phone number (with countary code) :")
-massage = input("Enter massage :")
-t1 = int(input("Enter hour in 24 hour format :"))
-t2 = int(input("Enter minute :"))
+number = st.text_input("Enter recevier's phone number (with countary code) ")
+massage = st.text_input("Enter massage ")
+t1 = st.selectbox("select hour", [i for i in range(24)])
+t2 = st.selectbox("select minute ", [j for j in range(60)])
 
-pywhatkit.sendwhatmsg(number, massage, t1, t2,20,True, 15)
+if st.button("send massage"):
+    if number and massage:
+        try:
+            pywhatkit.sendwhatmsg(number, massage, t1, t2,20,True, 15)
+            st.success("message sent successfully")
+        except Exception as e:
+            st.error(f"Failed tosent massage: {e}")
+
+    else:
+        (st.error("Please enter both phone number and message."))
+
